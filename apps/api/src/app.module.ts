770bbb55e89e9aa5dc366e9f778135d52d97f217
thiendang <thiendh310@gmail.com>
2024-06-26 14:35:26 +0700

    feat: created user model

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,21 +5,31 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 import { ConfigModule } from '@nestjs/config'
-import { UsersModule } from './users/users.module'
+import { PrismaModule } from './common/prisma/prisma.module'
+import { UsersModule } from './models/users/users.module'
+import { JwtModule } from '@nestjs/jwt'
+
+const MAX_AGE = 24 * 60 * 60
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: MAX_AGE },
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       introspection: true,
       fieldResolverEnhancers: ['guards'],
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       buildSchemaOptions: {
-        numberScalarMode: 'integer'
-      }
+        numberScalarMode: 'integer',
+      },
     }),
-    UsersModule
+    PrismaModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
